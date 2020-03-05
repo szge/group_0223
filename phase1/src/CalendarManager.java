1b556e0777bc10f3e34afbeadbd7db90f3d10fcb
danimhn <dani.mhn.2001@gmail.com>
2020-03-05 15:56:53 -0500

    Loading process done + get by date(DataManager)

--- phase1/src/CalendarManager.java
+++ phase1/src/CalendarManager.java
@@ -1,36 +1,36 @@
-import java.util.ArrayList;
-
-public class CalendarManager {
-
-    private CreatorManager creatorManager;
-    private DatabaseManager databaseManager;
-
-    public CalendarManager()
-    {
-        creatorManager = new CreatorManager();
-        databaseManager = new DatabaseManager();
-    }
-
-    public void createEvent()
-    {
-        e = creatorManager.createEvent();
-        databaseManager.addEvent(e);
-    }
-
-    public void createSeries()
-    {
-        s = creatorManager.createSeries();
-        databaseManager.addSeries(s);
-    }
-
-    public void viewEvents()
-    {
-        ArrayList<Event> allEvents = databaseManager.getAllEvents();
-        for(Event e: allEvents)
-        {
-            System.out.println(e.toString());
-            System.out.println();
-        }
-    }
-
-}
+//import java.util.ArrayList;
+//
+//public class CalendarManager {
+//
+//    private CreatorManager creatorManager;
+//    private DatabaseManager databaseManager;
+//
+//    public CalendarManager()
+//    {
+//        creatorManager = new CreatorManager();
+//        databaseManager = new DatabaseManager();
+//    }
+//
+//    public void createEvent()
+//    {
+//        e = creatorManager.createEvent();
+//        databaseManager.addEvent(e);
+//    }
+//
+//    public void createSeries()
+//    {
+//        s = creatorManager.createSeries();
+//        databaseManager.addSeries(s);
+//    }
+//
+//    public void viewEvents()
+//    {
+//        ArrayList<Event> allEvents = databaseManager.getAllEvents();
+//        for(Event e: allEvents)
+//        {
+//            System.out.println(e.toString());
+//            System.out.println();
+//        }
+//    }
+//
+//}
