4e3103fe82bd017276e7060712c824257b425b7e
arshammoradi <arsham.moradi@mail.utoronto.ca>
2020-03-06 09:52:46 -0500

    First version

--- phase1/src/AlertManager.java
+++ phase1/src/AlertManager.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+
 public class AlertManager {
+
+    private ArrayList<ArrayList<Alert>> serialAlerts;
+    private ArrayList
+
 }
--- phase1/src/MemoManager.java
+++ phase1/src/MemoManager.java
@@ -1,58 +1,28 @@
-import java.util.ArrayList;
+
+import java.util.HashMap;
 
 private class MemoManager {
 
-    public ArrayList<Memo> store; //holds a list of the Memos
+    HashMap<String, Memo> store; //holds a list of the Memos
 
     public MemoManager(){
-        this.store = new ArrayList<>();
+        this.store = new HashMap<String, Memo>();
     }
 
-    public int exists(String content){
-        //returns -1 if memo already exists, otherwise returns its id.
-        for (int i=0; i<this.store.size(); i++){
-            if (this.store.get(i).toString().equals(content)){return this.store.get(i).getId()};
+    public void addMemo(int id, String content){
+        if (this.store.containsKey(content)){
+            Memo newMemo = new Memo(content);
+            this.store.put(content, newMemo);
         }
-        return -1;
-    }
-
-    //For when the memo doesn't exist
-    public Memo addMemo(String content){
-        //returns Memo after its created
-        Memo newMemo = new Memo(content);
-        this.store.add(newMemo);
-        return newMemo;
-    }
-
-    //For when the memo already exists.
-    public Memo addMemo(int id){
-        //returns already exists in Memo after adding id of the new event to it's list of events.
-        for (int i=0; i<this.store.size(); i++){
-            if (this.store.get(i).getId() == id){
-                this.store.get(i).addEvent(id);
-                return this.store.get(i);
-            }
-        }
+        this.store.get(content).addEvent(id);
     }
 
     public void removeMemo(String content, int id){
-        for (int i=0; i<this.store.size(); i++){
-            if (store.get(i).toString().equals(content)) {
-                this.store.get(i).removeEvent(id); //Need a Memo method to remove the id of a Memo
-            }
+        this.store.get(content).removeEvent(id); //Need a Memo method to remove the id of a Memo
+        if (this.store.get(content).getEvents().size() == 0){
+            this.store.remove(content);
         }
         //Do we want to remove a memo that isn't associated with any events anymore?
     }
-
-    public void editMemo(String old, String text){
-        for (int i=0; i<this.store.size(); i++){
-            if (store.get(i).toString().equals(old)) {
-                this.store.get(i).editContent(text); //Need a Memo method to change the content of a Memo
-            }
-        }
-    }
-
-
-
 
 }
