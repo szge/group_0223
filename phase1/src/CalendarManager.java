dc0565755b9c22f71c9de6eefa2361bf6215a41d
alextobias <alexanderleetobias@gmail.com>
2020-03-07 15:50:09 -0500

    Commented + Updated methods; getEventsByDate, getAlerts, deleteEvents

--- phase1/src/CalendarManager.java
+++ phase1/src/CalendarManager.java
@@ -1,88 +1,122 @@
-//import java.util.ArrayList;
-//
-//public class CalendarManager {
-//
-//    private CreatorManager creatorManager;
-//    private DatabaseManager databaseManager;
-//
-//    public CalendarManager()
-//    {
-//        creatorManager = new CreatorManager();
-//        databaseManager = new DatabaseManager();
-//    }
-//
-//    public void createEvent()
-//    {
-//        e = creatorManager.createEvent();
-//        databaseManager.addEvent(e);
-//    }
-//
-//    public void createSeries()
-//    {
-//        s = creatorManager.createSeries();
-//        databaseManager.addSeries(s);
-//    }
-//
-//    public void viewEvents()
-//    {
-//        ArrayList<Event> allEvents = databaseManager.getAllEvents();
-//        for(Event e: allEvents)
-//        {
-//            System.out.println(e.toString());
-//            System.out.println();
-//        }
-//    }
-//
-//}
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class CalendarManager {
     public UserManager userMg = new UserManager();
 
+    /**
+     * Logs in this user.
+     * Uses userMg to confirm if this user exists
+     * Asks DataManager to log this user in and retrieve their events/data etc
+     * @param user
+     * @param pass
+     * @return
+     * * the ID of the user, if successful
+     * * -1 if user does not exist at all
+     * * -2 if username exists but password is wrong.
+     */
     public int login(String user, String pass){
         int code = userMg.login(user, pass);
         if(code > 0){
             // TODO: get this to work
-//            DataManager.login(user);
+            // DataManager.login(user);
         }
         return code;
     }
 
+    /**
+     * Creates a new user via UserManager
+     * @param user the username of the user to create
+     * @param pass the password of the user to create
+     * @return
+     * TRUE if a new user was successfully created
+     * FALSE if a user with that username exists already
+     */
     public boolean createNewUser(String user, String pass){
         /* TODO: create a method createNewUser() in DataManager to correctly set up a new user for access */
         // DataManager.createNewUser(user)
         return userMg.createNewUser(user, pass);
     }
 
+    /**
+     * Deletes user by the Specified ID.
+     * @param userID the ID of the user to remove
+     */
     public void deleteUserID(int userID) {
         userMg.deleteUserByName(userID);
     }
 
+    /**
+     * Gets all Events in our calendar
+     * @return an array of all events (from DataManager)
+     */
     public Event[] getEvents() {
         /* TODO: theoretically DataManager should have a getEvents() method and this will just call that and return */
+        // return DataManager.getEvents();
         return new Event[0];
     }
 
+    /**
+     * Gets the Event matching the specified ID
+     * @param id the ID of the event to retrieve
+     * @return Event matching the ID
+     */
     public Event getEventByID(String id) {
         /* TODO: Get this event by id */
+        // return DataManager.getEventByID(id);
         return null;
     }
 
+    /**
+     * @author Alex, Jonathan
+     * @param ids an array of IDs representing the events to delete
+     * @return void
+     * Iterates through list of event IDs and gets DataManager to delete those events.
+     */
     public void deleteEvents(int[] ids) {
+        /*
+        for(int i: ids)
+        {
+            DataManager.deleteEvent(i);
+        }
+        */
+        // TODO: Need to confirm that DataManager.deleteEVent will automatically delete associated alerts
+
         /* TODO: Given a list of event ids, delete all the events corresponding to this user's valid event ids.
         *   If a given ID is not valid, do nothing. Make sure to also delete all the alerts associated with each event*/
     }
 
 
-    public Event[] getEventsByDate(int[] delete) {
-        /* TODO: Given an input of a 3 element int input of the format [day, month, year], call the getEventsByDay()
-        *   method in DataManager and return */
-        return new Event[0];
+    /**
+     * @author Jonathan, Alex
+     * Given an input of a desired date, returns the list of matching Events from DataManager
+     *
+     * @param dateInfo A 3 element int input of the format [day, month, year]
+     * @return array of Events matching the search date (from DataManager)
+     */
+    public ArrayList<Event> getEventsByDate(int[] dateInfo) {
+        // Does the conversion from the array to LocalDate object
+        int dayOfMonth = dateInfo[0];
+        int month = dateInfo[1];
+        int year = dateInfo[2];
+        LocalDate searchDate = LocalDate.of(year, month, dayOfMonth);
+        return DataManager.getEventsByDate(searchDate);
+        // Use the below instead for purely testing
+        // return new ArrayList<Event>(0);
     }
 
 
-    public Alert[] getAlerts() {
+    /**
+     * @author Alex, Jonathan
+     * @return ArrayList of all Alerts
+     */
+    public ArrayList<Alert> getAlerts() {
         /* TODO: theoretically DataManager should have a getAlerts() method and this will just call that and return */
-        return new Alert[0];
+        ArrayList<Alert> alerts = DataManager.getAlerts();
+        return alerts;
+
+        // Use the below instead for purely testing
+        // return new ArrayList<Event>(0);
     }
 }
 
