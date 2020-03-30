483d73b1b857c4468d6737d8ff009e5b90e992c0
arshammoradi <arsham.moradi@mail.utoronto.ca>
2020-03-30 01:24:32 -0600

    Memo manager updated

--- phase1/src/MemoManager.java
+++ phase1/src/MemoManager.java
@@ -4,31 +4,48 @@ import java.util.ArrayList;
 
 public class MemoManager {
 
-    private HashMap<String, Memo> store; //holds a list of the Memos
+    private ArrayList<Memo> store; //holds a list of the Memos
 
-    public MemoManager(ArrayList<>){
-        this.store = new HashMap<String, Memo>();
+    public MemoManager(ArrayList<> store){
+        this.store = store;
     }
 
     public Memo addMemo(int id, String content){
         //create a memo for event with id
-        Memo newMemo = null;
-        if (!this.store.containsKey(content)){
+        Memo newMemo = this.getContent(content);
+        if (!this.contains(content)){
             newMemo = new Memo(content);
-            CalendarDataFacade.addMemo(newMemo);
-            this.store.put(content, newMemo);
+            this.store.add(newMemo);
         }
-        this.store.get(content).addEvent(id);
+        newMemo.addEvent(id);
         return newMemo;
     }
 
     public void removeMemo(String content, int id){
         //remove the id of an event from a memo
-        this.store.get(content).removeEvent(id); //Need a Memo method to remove the id of a Memo
-        if (this.store.get(content).getEvents().size() == 0){
+        this.getContent(content).removeEvent(id); //Need a Memo method to remove the id of a Memo
+        if (this.getContent(content).getEvents().size() == 0){
             this.store.remove(content);
         }
         //Do we want to remove a memo that isn't associated with any events anymore?
+    }
+
+    public boolean contains(String content){
+        for (int i = 0; i < this.store.size(); i++) {
+            if (this.store.get(i).toString().equals(content)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Memo getContent(String content){
+        for (int i = 0; i < this.store.size(); i++) {
+            if (this.store.get(i).toString().equals(content)){
+                return this.store.get(i);
+            }
+        }
+        return null;
     }
 
     public void removeEventMemo(Memo memo, int id){
